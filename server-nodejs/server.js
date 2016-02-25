965de42018622eca57ae33d68948670b809d3d6d
mixgame <mixgame@foxmail.com>
2016-02-25 09:06:56 +0800

    自己试做服务器

--- server-nodejs/server.js
+++ server-nodejs/server.js
@@ -1,15 +1,12 @@
-'use strict';
-var AV = require('leanengine');
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
-var app = require('./app');
-// 端口一定要从环境变量 `LC_APP_PORT` 中获取。
-// LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LC_APP_PORT || 3000);
-app.listen(PORT, function () {
-    console.log('Node app is running, port:', PORT);
+/// <reference path="../typings/express/express.d.ts" />
+var express = require('express');
+var app = express();
+var admin = express();
+admin.get('/', function (req, res, next) {
+    console.log(admin.mountpath);
+    res.send('admin homepage');
 });
+app.use('/admin', admin);
+app.use(express.static(__dirname + "/public"));
+console.log("dir:" + __dirname);
+app.listen(3000);
--- server-nodejs/server.ts
+++ server-nodejs/server.ts
@@ -1,19 +1,16 @@
-'use strict';
-var AV = require('leanengine');
+/// <reference path="../typings/express/express.d.ts" />
+import express = require('express');
+var app = express();
+var admin = express();
 
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
-
-var app = require('./app');
-
-// 端口一定要从环境变量 `LC_APP_PORT` 中获取。
-// LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LC_APP_PORT || 3000);
-app.listen(PORT, function () {
-    console.log('Node app is running, port:', PORT);
+admin.get('/', function (req:express.Request,res:express.Response,next:express.NextFunction) {
+    console.log(admin.mountpath);
+    res.send('admin homepage');
 });
+
+app.use('/admin',admin);
+
+app.use(express.static(__dirname+"/public"));
+console.log("dir:"+__dirname);
+
+app.listen(3000);
